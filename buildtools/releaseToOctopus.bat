3f8d65bd201c51fd18090307ef0d48f6f3d401e5
Artur Teregulov <sliceice@gmail.com>
2015-11-02 19:49:41 +0300

    remove scrollbar

--- buildtools/releaseToOctopus.bat
+++ buildtools/releaseToOctopus.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
 pushd ..
-powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 54 -BuildScript "buildtools\releaseToOctopus.fsx"
+powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 55 -BuildScript "buildtools\releaseToOctopus.fsx"
 popd
 popd
