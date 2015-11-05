b6a13444fef32eca3122a06171323b789042b042
Artur Teregulov <sliceice@gmail.com>
2015-11-05 19:23:17 +0300

    BuildNumber

--- buildtools/releaseToOctopus.bat
+++ buildtools/releaseToOctopus.bat
@@ -1,5 +1,5 @@
 pushd %~dp0
 pushd ..
-powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 61 -BuildScript "buildtools\releaseToOctopus.fsx"
+powershell buildtools\build.ps1 -MainVersion 1.2.0 -BuildNumber 63 -BuildScript "buildtools\releaseToOctopus.fsx"
 popd
 popd
