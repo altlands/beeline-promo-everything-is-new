51194eff578a053441527b9c8a29da6a72743380
ateregulov <sliceice@gmail.com>
2015-11-17 18:33:16 +0300

    UssApiClient minification

--- Promo.EverythingIsNew.WebApp/Models/UsssClient.cs
+++ Promo.EverythingIsNew.WebApp/Models/UsssClient.cs
@@ -18,61 +18,24 @@ using System.Web;
 
 namespace Promo.EverythingIsNew.WebApp.Models
 {
-
-
     public class UssApiClient
     {
-        public UssClientConfig Config { get; set; }
         private readonly HttpClient _httpClient;
-        private readonly HttpClient _httpLongClient;
 
-
-        public UssApiClient()
-            : this(null, null)
+        public UssApiClient(DelegatingHandler delegatingHandler)
         {
-        }
-
-        public UssApiClient(DelegatingHandler delegatingHandler, UssClientConfig config = null)
-        {
-            Config = config ?? UssClientConfig.Current;
-            HttpMessageHandler handler = new MainUssHandler(Config, delegatingHandler);
-
-            _httpClient = new HttpClient(handler) { BaseAddress = Config.EndpointAddress, Timeout = Config.HttpClientTimeout };
-            _httpLongClient = new HttpClient(handler) { BaseAddress = Config.EndpointAddress, Timeout = Config.HttpClientLongTimeout };
+            HttpMessageHandler handler = new MainUssHandler(delegatingHandler);
+            _httpClient = new HttpClient(handler);
         }
 
         public async Task<HttpResponseMessage> ProcessRequestAsync(UssApiMethod method)
         {
             var handler = method.GetResponseHandler();
-            var operationId = Guid.NewGuid();
-
-            Stopwatch time = Stopwatch.StartNew();
-
             using (var request = method.CreateRequest())
             {
-                if (method.IsPasswordSendingMethod())
-                {
-                    UssEvents.Log.CallingUss(request.RequestUri.OriginalString, operationId);
-                }
-                else
-                    UssEvents.Log.CallingUss(request.RequestUri.OriginalString, operationId, method.StringContent);
-                string cookieLog = null;
-                IEnumerable<string> cookies;
-                if (request.Headers.TryGetValues("Cookie", out cookies))
-                    cookieLog = string.Join(", ", cookies);
-
-                UssEvents.Log.CallingUss(request.RequestUri.OriginalString, operationId, method.StringContent, cookieLog);
-
                 Task<HttpResponseMessage> responseTask;
-
-                if (method.IsLongRunning)
-                {
-                    responseTask = _httpLongClient.SendAsync(request);
-                }
-                else
-                    responseTask = _httpClient.SendAsync(request);
-
-                return await handler.HandleResponse(responseTask, request.RequestUri.OriginalString, operationId, time);
+                responseTask = _httpClient.SendAsync(request);
+                return await handler.HandleResponse(responseTask, request.RequestUri.OriginalString);
             }
         }
 
@@ -81,136 +44,49 @@ namespace Promo.EverythingIsNew.WebApp.Models
             var handler = method.GetResponseHandler();
             using (var request = method.CreateRequest())
             {
-                var operationId = Guid.NewGuid();
-
-                if (method.IsPasswordSendingMethod())
-                {
-                    UssEvents.Log.CallingUss(request.RequestUri.OriginalString, operationId);
-                }
-                else
-                    UssEvents.Log.CallingUss(request.RequestUri.OriginalString, operationId, method.StringContent);
                 return await _httpClient.SendAsync(request);
             }
         }
     }
 
 
-
-
-    public class UssClientConfig
-    {
-        private static readonly Lazy<UssClientConfig> _current = new Lazy<UssClientConfig>(
-            () =>
-            {
-                var configuration = (IDictionary)ConfigurationManager.GetSection("uss");
-                var current = new UssClientConfig()
-                {
-                    EndpointAddress = new Uri(configuration["endpointAddress"].ToString()),
-                    HttpClientTimeout = TimeSpan.Parse(configuration["invokeTimeout"].ToString()),
-                    HttpClientLongTimeout = configuration["invokeLongTimeout"] == null ? TimeSpan.Parse(configuration["invokeTimeout"].ToString()) : TimeSpan.Parse(configuration["invokeLongTimeout"].ToString()),
-                    Token = configuration["token"].ToString(),
-                    Signature = configuration["signature"].ToString(),
-                    ClientType = configuration["clientType"] == null ? null : configuration["clientType"].ToString()
-                };
-                return current;
-            });
-
-        public static UssClientConfig Current
-        {
-            get
-            {
-                return _current.Value;
-            }
-        }
-
-        public Uri EndpointAddress { get; set; }
-        public TimeSpan HttpClientTimeout { get; set; }
-        public TimeSpan HttpClientLongTimeout { get; set; }
-        public String Token { get; set; }
-        public String Signature { get; set; }
-        public String ClientType { get; set; }
-    }
-
     public class MainUssHandler : DelegatingHandler
     {
-        private readonly UssClientConfig _config;
-        private readonly string _token;
-        private readonly string _tokenParamName;
-        private readonly bool _useHeaderInsteadOfCookie;
-
-        public MainUssHandler(UssClientConfig config, DelegatingHandler delegatingHandler = null)
+        public MainUssHandler(DelegatingHandler delegatingHandler = null)
         {
-            _config = config;
-
             HttpMessageHandler inner = new HttpClientHandler()
             {
                 UseCookies = false
             };
-
-            if (delegatingHandler != null)
-            {
-                if (delegatingHandler.InnerHandler != null) throw new ArgumentException("delegatingHandler has non-null InnerHandler", "delegatingHandler.InnerHandler");
-                delegatingHandler.InnerHandler = inner;
-                inner = delegatingHandler;
-            }
-
-            inner = new UssAuthHandler(_config.Token)
-            {
-                InnerHandler = inner
-            };
-
-            this.InnerHandler = inner;
         }
 
-        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
-            CancellationToken cancellationToken)
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
-
-            if (_config.ClientType != null && !request.Headers.Any(h => h.Key.Equals("Client-Type")))
-            {
-                request.Headers.Add("Client-Type", _config.ClientType);
-            }
-
             var response = await base.SendAsync(request, cancellationToken);
-
             return response;
         }
     }
 
     public class UssApiMethod
     {
-        private readonly IUssHashFunction _hashFunction;
-        public string Version { get; protected set; }
         public string BaseUri { get; protected set; }
         public string MethodName { get; protected set; }
         public HttpMethod HttpMethod { get; protected set; }
         public NameValueCollection QueryStringParams { get; set; }
         public HttpContent Body { get; set; }
         public string StringContent { get; set; }
-        public bool IsLongRunning { get; set; }
 
-
-        public UssApiMethod(string methodName, HttpMethod httpMethod, IUssHashFunction hashFunction = null, string version = "v2.0", string baseUri = "api")
+        public UssApiMethod(string methodName, HttpMethod httpMethod, string baseUri = "api")
         {
-            //_hashFunction = hashFunction ?? UssHashFunction.StandartHashFunction;
             BaseUri = baseUri;
-            Version = version;
             MethodName = methodName;
             HttpMethod = httpMethod;
-
-            IsLongRunning = false;
-            //QueryStringParams = new QueryStringParser(string.Empty);
             Body = null;
         }
 
         protected virtual string GetBaseUriForMethod()
         {
-            return BaseUri + "/" + Version + "/" + MethodName;
-        }
-
-        protected virtual string ComputeHash(params string[] args)
-        {
-            return _hashFunction.ComputeHash(String.Concat(args));
+            return BaseUri + "/" + "/" + MethodName;
         }
 
         protected virtual NameValueCollection GetQueryParams()
@@ -232,21 +108,6 @@ namespace Promo.EverythingIsNew.WebApp.Models
             return null;
         }
 
-        private string GetHashForRequestInternal(NameValueCollection queryStringParams)
-        {
-            var paramsToHash = new NameValueCollection(queryStringParams);
-            paramsToHash.Remove("client");
-            paramsToHash.Remove("hash");
-            var stringToHash = string.Join("", paramsToHash.AllKeys.Select(key => ProceedStringForHash(paramsToHash[key])));
-            return _hashFunction.ComputeHash(stringToHash);
-        }
-
-        private string ProceedStringForHash(string str)
-        {
-
-            return str;
-        }
-
         public virtual ResponseHandler GetResponseHandler()
         {
             return new EmptyResponseHandler();
@@ -260,9 +121,6 @@ namespace Promo.EverythingIsNew.WebApp.Models
                 var requsetUri = GetBaseUriForMethod();
                 var queryString = GetQueryParams();
                 if (QueryStringParams.HasKeys()) queryString.Add(QueryStringParams);
-
-                var hash = GetHashForRequest() ?? GetHashForRequestInternal(queryString);
-                if (!String.IsNullOrEmpty(hash)) queryString["hash"] = hash;
 
                 if (queryString.HasKeys()) requsetUri = requsetUri + "?" + queryString;
                 _queryString = requsetUri;
@@ -303,13 +161,6 @@ namespace Promo.EverythingIsNew.WebApp.Models
 
 
 
-    public static class Extensions
-    {
-        public static bool IsPasswordSendingMethod(this object method)
-        {
-            return false;
-        }
-    }
 
     public static class UssEvents
     {
@@ -339,41 +190,10 @@ namespace Promo.EverythingIsNew.WebApp.Models
 
     public class UssAuthHandler : DelegatingHandler
     {
-        private readonly string _token;
-        private readonly string _tokenParamName;
-        private readonly bool _useHeaderInsteadOfCookie;
-
-        public UssAuthHandler(string token, string tokenParamName = "token", bool useHeaderInsteadOfCookie = false)
-        {
-            _token = token;
-            _tokenParamName = tokenParamName;
-            _useHeaderInsteadOfCookie = useHeaderInsteadOfCookie;
-        }
-
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
             CancellationToken cancellationToken)
         {
-            if (_useHeaderInsteadOfCookie)
-            {
-                if (!request.Headers.Contains(_tokenParamName)) request.Headers.Add(_tokenParamName, _token);
-            }
-            else
-            {
-                if (request.Headers.Contains("Cookie"))
-                {
-                    var existingCookies = request.Headers.First(h => h.Key == "Cookie").Value.ToList();
-
-                    if (!existingCookies.Any(c => c.StartsWith(_tokenParamName + "=", StringComparison.CurrentCultureIgnoreCase)))
-                        existingCookies.Add(String.Format("{0}={1}", _tokenParamName, _token));
-
-                    existingCookies.RemoveAll(c => c == (_tokenParamName + "="));  //remove empty token
-
-                    request.Headers.Remove("Cookie");
-                    request.Headers.Add("Cookie", string.Join(";", existingCookies));
-                }
-                else
-                    request.Headers.Add("Cookie", String.Format("{0}={1}", _tokenParamName, _token));
-            }
+            
 
             var response = await base.SendAsync(request, cancellationToken);
 
@@ -389,7 +209,7 @@ namespace Promo.EverythingIsNew.WebApp.Models
     public abstract class ResponseHandler
     {
         public abstract bool ShouldHandleResponse(HttpResponseMessage response);
-        public abstract Task<HttpResponseMessage> HandleResponse(Task<HttpResponseMessage> responseTask, string uri, Guid operationId, Stopwatch time);
+        public abstract Task<HttpResponseMessage> HandleResponse(Task<HttpResponseMessage> responseTask, string uri);
     }
 
     public class EmptyResponseHandler : ResponseHandler
@@ -400,7 +220,7 @@ namespace Promo.EverythingIsNew.WebApp.Models
             return false;
         }
 
-        public async override Task<HttpResponseMessage> HandleResponse(Task<HttpResponseMessage> responseTask, string uri, Guid operationId, Stopwatch time)
+        public async override Task<HttpResponseMessage> HandleResponse(Task<HttpResponseMessage> responseTask, string uri)
         {
             HttpResponseMessage response = null;
             string content = null;
@@ -409,7 +229,6 @@ namespace Promo.EverythingIsNew.WebApp.Models
             {
                 response = await responseTask;
                 content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                time.Stop();
                 if (!content.Contains("\"code\":" + ((int)UssStatusCode.SUCCESS).ToString())) //Йа костыль!
                 {
                     var json = JsonConvert.DeserializeObject<dynamic>(content);
@@ -419,11 +238,9 @@ namespace Promo.EverythingIsNew.WebApp.Models
                     }
                 }
 
-                UssEvents.Log.UssResponse(uri, operationId, time.Elapsed, content, (int)response.StatusCode);
             }
             catch (Exception e)
             {
-                time.Stop();
                 
                 throw;
             }
@@ -472,4 +289,57 @@ namespace Promo.EverythingIsNew.WebApp.Models
         CTN_NOT_FOUND = 20005,
         FORBIDDEN = 20006,
     };
+
+
+
+
+    public class CreateSsoRequestMethod : GetDataMethod<Int64>
+    {
+        public string UssLogin { get; set; }
+        public string LinkedAccountLogin { get; set; }
+        public string Nickname { get; set; }
+        public bool IsInvite { get; set; }
+
+        public CreateSsoRequestMethod(string ussLogin, string linkedAccountLogin, bool isInvite, string nickName = null)
+            : base("sso/linkage/request/creation")
+        {
+            HttpMethod = HttpMethod.Post;
+            UssLogin = ussLogin;
+            LinkedAccountLogin = linkedAccountLogin;
+            Nickname = nickName ?? "";
+            IsInvite = isInvite;
+        }
+
+        protected override string GetStringBodyContent()
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                requestType = IsInvite ? 1 : 0,
+                linkedAccountLogin = LinkedAccountLogin,
+                login = UssLogin,
+                nickName = Nickname
+            });
+        }
+
+        public override async Task<Int64> ParseResponseAsync(HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<dynamic>(content).requestId;
+        }
+    }
+
+
+    public class GetDataMethod<T> : UssApiMethod
+    {
+        public GetDataMethod(string methodName) : base(methodName, HttpMethod.Get)
+        {
+        }
+
+        public virtual async Task<T> ParseResponseAsync(HttpResponseMessage response)
+        {
+            var content = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+            return JsonConvert.DeserializeObject<T>(content, new JsonSerializerSettings() { NullValueHandling = NullValueHandling.Ignore });
+        }
+
+    }
 }
